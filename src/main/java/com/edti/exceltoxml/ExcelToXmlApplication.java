d0e2a7f1997d5dfc5c4feb9470c6e2086ff92882
vasvince98 <vas.vince98@gmail.com>
2022-08-23 10:24:27 +0200

    first commit

---
+++ src/main/java/com/edti/exceltoxml/ExcelToXmlApplication.java
@@ -0,0 +1,13 @@
+package com.edti.exceltoxml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExcelToXmlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExcelToXmlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/edti/exceltoxml/ExcelToXmlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edti.exceltoxml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExcelToXmlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
