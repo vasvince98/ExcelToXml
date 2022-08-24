0fdc77553c1cdb2da24f8a38acef4c8aec8f5a48
vasvince98 <vas.vince98@gmail.com>
2022-08-24 10:21:28 +0200

    Update QuestionService.java

--- src/main/java/com/edti/exceltoxml/Services/QuestionService.java
+++ src/main/java/com/edti/exceltoxml/Services/QuestionService.java
@@ -1,4 +1,17 @@
 package com.edti.exceltoxml.Services;
 
-public class QuestionService {
+import com.edti.exceltoxml.Models.Quiz;
+
+import java.io.File;
+
+public class QuestionService implements IQuestionService {
+    @Override
+    public Quiz createObjectFromExcel(File file) {
+        return null;
+    }
+
+    @Override
+    public String createXmlFromObject(Quiz quiz) {
+        return null;
+    }
 }
